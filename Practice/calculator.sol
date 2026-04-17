48e839125584a8ed1eb9a4b61afd4f96badd32ab
GurdilSandhu <gurdilsingh10@gmail.com>
2026-04-17 11:45:38 +0530

    commit

--- Practice/calculator.sol
+++ Practice/calculator.sol
@@ -8,8 +8,23 @@ contract Calculator{
 
     event Calculation(string operation, uint value, uint result);
 
-    function setValue(uint _value) public{
-        result = _value;
-        emit  Calculation("SET", _value, result);
+    function Addvalue(uint _value) public{
+        result += _value;
+        emit Calculation("ADD", _value, result);
     }
-}
+
+    function Multiplyvalue(uint _value) public{
+        result *= _value;
+        emit Calculation("MULTI", _value, result);
+    }
+
+    function Dividevalue(uint _value) public{
+        result/= _value;
+        emit Calculation("DIV", _value, result);
+    }
+
+    function Subvalue(uint _value) public{
+        result-= _value;
+        emit Calculation("SUB", _value, result);
+    }
+} 
