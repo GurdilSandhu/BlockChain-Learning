7d528f74cd3fb9974d92ba50f266be6fc65737f7
GurdilSandhu <gurdilsingh10@gmail.com>
2026-04-17 12:14:15 +0530

    commit

--- Practice/IncreDecre.sol
+++ Practice/IncreDecre.sol
@@ -2,14 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract IncreDecre{
-     int public value = 0;
+     uint public value = 0;
 
-     function Increment() public returns(int){
+     function Increment() public returns(uint){
         value++;
         return value;
      }
 
-      function Decrement() public returns(int){
+      function Decrement() public returns(uint){
         value--;
         return value;
      }
--- Practice/calculator.sol
+++ Practice/calculator.sol
@@ -4,26 +4,26 @@ pragma solidity ^0.8.0;
 
 contract Calculator{
 
-    int public result;
+    uint public result;
 
-    event Calculation(string operation, int value, int result);
+    event Calculation(string operation, uint value, uint result);
 
-    function Addvalue(int _value) public{
+    function Addvalue(uint _value) public{
         result += _value;
         emit Calculation("ADD", _value, result);
     }
 
-    function Multiplyvalue(int _value) public{
+    function Multiplyvalue(uint _value) public{
         result *= _value;
         emit Calculation("MULTI", _value, result);
     }
 
-    function Dividevalue(int _value) public{
+    function Dividevalue(uint _value) public{
         result/= _value;
         emit Calculation("DIV", _value, result);
     }
 
-    function Subvalue(int _value) public{
+    function Subvalue(uint _value) public{
         result-= _value;
         emit Calculation("SUB", _value, result);
     }
