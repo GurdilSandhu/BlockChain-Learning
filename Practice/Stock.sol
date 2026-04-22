bd8c83863f05432b2f9a8b83ff9501a4fde1e774
GurdilSandhu <gurdilsingh10@gmail.com>
2026-04-22 12:04:02 +0530

    commit

---
+++ Practice/Stock.sol
@@ -0,0 +1,60 @@
+//  Inventory Management with Access Restrictions
+// Problem Statement:
+// Create a contract to manage product stock. The contract owner should be able to add 
+// and update product quantities. Each product must have a valid stock value. 
+//Maintain a list of all product identifiers and prevent duplicate entries. 
+//Users should be able to reduce stock when purchasing, ensuring stock never goes negative.
+//  Limited Slot Registration System
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Inventory_Management{
+    address owner;
+    uint[] productID;
+    mapping (uint => uint) product_stock;
+    mapping (uint => bool) isExisted;
+
+    constructor(){
+        owner=msg.sender;
+    }
+
+    modifier checkOwner{
+        require(owner==msg.sender,"Access Denied");
+        _;
+    }
+
+    function addProduct(uint id,uint quantity) public checkOwner{
+        require(!isExisted[id],"Id already exist");
+       
+        productID.push(id);
+        product_stock[id]=quantity;
+        isExisted[id]=true;
+    }
+
+    function updateProduct(uint id,uint quantity) public checkOwner{
+        require(!isExisted[id],"Id already exist");
+       
+        productID.push(id);
+        product_stock[id]=quantity;
+        isExisted[id]=true;
+    }
+
+    function buyProduct(uint id, uint quantity) public {
+        require(isExisted[id],"Invalid Id");
+        require(product_stock[id]>=quantity,"Invalid quantity");
+     
+        product_stock[id]-=quantity;
+
+        assert(product_stock[id]>=0);
+    }
+
+     function getFullDetails() public view returns (uint[] memory, uint[] memory) {
+        uint[] memory all_products=new uint[](productID.length);
+        for (uint i = 0; i < productID.length; i++) {
+            all_products[i]= product_stock[productID[i]];
+        }
+
+        return (productID, all_products);
+    }
+}
