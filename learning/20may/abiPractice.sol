51fd37c9f6216a66072f37c15b3de28b9f014abc
GurdilSandhu <gurdilsingh10@gmail.com>
2026-05-25 11:46:23 +0530

    commit

--- learning/20may/abiPractice.sol
+++ learning/20may/abiPractice.sol
@@ -50,15 +50,30 @@ contract abiPrac{
         return string(abi.encodePacked("City: ",city," "," Country: ",country));
     }
 
-    function generateStudentId(string memory sName,uint roll)public pure returns(bytes32){
-
-        return keccak256(abi.encodePacked(Strings.toString(roll),sName));
-    }
-
      
 
     function FLDOB_Combine(string memory _fName,string memory lName,uint DOB) public pure returns(string memory){
         return string(abi.encodePacked(_fName,lName,Strings.toString(DOB)));
     }
 
+    function hashBoth(
+        uint _num,
+        string memory _word
+    )
+        public
+        pure
+        returns(bytes32)
+    {    
+        //0x0a0e9ab94d02b5337561d85c048707c3111b707cfc6e9f6063953a97639c67a4
+        //0x3277411ad36d533d3789b37c652a5387b38c29322704fa30f36fad9bfc05bf3a
+        return keccak256(abi.encodePacked(_word,_num));
+}
+function generateStudentId(string memory sName,uint roll)public pure returns(bytes32){
+
+        return keccak256(abi.encodePacked(roll,sName));
+    }
+    function generateHash(string memory name)public view returns(bytes32){
+
+        return keccak256(abi.encodePacked(name, block.timestamp, msg.sender));
+    }
 }
