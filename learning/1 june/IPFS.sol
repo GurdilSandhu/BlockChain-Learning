0babb1c185f1f4216a0ce29ce3961316c99439a7
GurdilSandhu <gurdilsingh10@gmail.com>
2026-07-21 11:26:23 +0530

    commit

--- learning/1 june/IPFS.sol
+++ learning/1 june/IPFS.sol
@@ -1,18 +1,14 @@
-// cid store in variable
-// get file url
-
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract IPFS{
-    string cId;
-
-    function addCid(string memory _cId)public {
-        cId = _cId;
+    string CID;
+ 
+    function addCid(string memory _CID)public{
+        CID = _CID;
     }
     function geturl()public view returns(string memory){
-        string memory mainURL = string(abi.encodePacked("https://ipfs.io/ipfs/",cId));
+        string memory mainURL = string(abi.encodePacked("https://ipfs.io/ipfs/",CID));
     
         return mainURL;
     }
