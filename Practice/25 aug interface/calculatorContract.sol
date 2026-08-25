da241f4b0cb83d3109797f78993aaca4febc794b
GurdilSandhu <gurdilsingh10@gmail.com>
2026-08-25 13:06:10 +0530

    commit

--- Practice/25 aug interface/calculatorContract.sol
+++ Practice/25 aug interface/calculatorContract.sol
@@ -4,23 +4,4 @@ pragma solidity ^0.8.20;
 import "./calculatorInterface.sol";
 
 contract calculatorContract is calculatorInterface{
-
-    function Cadd(uint a) public{
-        this.add(a);
-    }
-    function Csub(uint a) public{
-        this.sub(a);
-    }
-    function Cmulti(uint a) public{
-        this.multi(a);
-    }
-    function Cdivide(uint a) public{
-        this.divide(a);
-    }
-    function Cresult() public view returns(uint){
-        return this.result();
-    }
-    function Cclear() public{
-        this.clear();
-    }
 }
